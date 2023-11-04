dbc1364cf9ca06a0f0fa24881f6abad54f435625
Bram <bram@haslux.net>
2023-11-04 21:53:34 +0700

    Added some comments/questions for Gene

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,5 +1,8 @@
 
 
+// BRAM: Do you understand what is going on here, why are we doing
+// BRAM: { board, handleTabClick } - before when you had a function you didn't have the {}
+// BRAM: It is correct, but do you understand the why behind it ;)
 function GameBoard({ board , HandleTabClick }){
     return (
         <div className="grid grid-cols-3 grid-rows-3  gap-[5px] p-[5px] rounded-[10px] bg-[snow]">
--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,3 +1,4 @@
+
 function Title(){
     return (
         <p className="text-center text-[snow] text-[60px] p-[10px]">Tic Tac Toe</p>
